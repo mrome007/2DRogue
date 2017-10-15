c876e19d7ddc86befaad6bc60845a998def2f95f
mrome007 <mrome007unity@gmail.com>
2017-10-15 14:57:40 -0700

    Added new sprite and implement movement with acceleration/deceleration.
    TODO: bound player to camera bounds

--- Assets/Scripts/RogueMovement.cs
+++ Assets/Scripts/RogueMovement.cs
@@ -7,14 +7,36 @@ public class RogueMovement : MonoBehaviour
 	#region Inspector Fields
 
 	/// <summary>
-	/// The speed.
+	/// The maximum speed.
 	/// </summary>
 	[SerializeField]
-	private float speed = 5.0f;
+	private float maximumSpeed = 6.0f;
+
+	/// <summary>
+	/// The acceleration.
+	/// </summary>
+	[SerializeField]
+	private float acceleration = 1.0f;
+
+	/// <summary>
+	/// The deceleration.
+	/// </summary>
+	[SerializeField]
+	private float deceleration = 1.0f;
 
 	#endregion
 
 	#region Private Fields
+
+	/// <summary>
+	/// The horizontal speed.
+	/// </summary>
+	private float horizontalSpeed;
+
+	/// <summary>
+	/// The vertical speed.
+	/// </summary>
+	private float verticalSpeed;
 
 	/// <summary>
 	/// The movement vector.
@@ -28,6 +50,8 @@ public class RogueMovement : MonoBehaviour
 	private void Start()
 	{
 		movementVector = Vector3.zero;
+		horizontalSpeed = 0f;
+		verticalSpeed = 0f;
 	}
 
 	private void Update()
@@ -35,10 +59,51 @@ public class RogueMovement : MonoBehaviour
 		var h = Input.GetAxisRaw("Horizontal");
 		var v = Input.GetAxisRaw("Vertical");
 
-		movementVector.x = h;
-		movementVector.y = v;
-		transform.Translate(movementVector * speed * Time.deltaTime);
+		horizontalSpeed = GetSpeed(h, horizontalSpeed);
+		verticalSpeed = GetSpeed(v, verticalSpeed);
+
+		movementVector.x = horizontalSpeed;
+		movementVector.y = verticalSpeed;
+		transform.Translate(movementVector);
 	}
 
 	#endregion
+
+	/// <summary>
+	/// Gets the speed.
+	/// </summary>
+	/// <returns>The speed.</returns>
+	/// <param name="axis">Axis.</param>
+	/// <param name="speed">Speed.</param>
+	float GetSpeed(float axis, float speed)
+	{
+		float result = speed;
+		//move with acceleration
+		if(axis != 0)
+		{
+			var resultSpeed = result + (axis * acceleration * Time.deltaTime);
+			if(Mathf.Abs(resultSpeed) < maximumSpeed)
+			{
+				result = result + (axis * acceleration * Time.deltaTime);
+			}
+			else
+			{
+				result = maximumSpeed * axis;
+			}
+		}
+		//stop and decelerate.
+		else
+		{
+			var sign = Mathf.Sign(result);
+			if(Mathf.Abs(result) > 0.01)
+			{
+				result = result + (-sign * deceleration * Time.deltaTime);
+			}
+			else
+			{
+				result = 0f;
+			}
+		}
+		return result;
+	}
 }
